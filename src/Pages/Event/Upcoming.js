fd37b21399906128fd8f1b9dc586ac2a05a56dcf
martinyis <martinyis11@gmail.com>
2023-03-29 15:55:32 -0400

    Add mobile size track

--- src/Pages/Event/Upcoming.js
+++ src/Pages/Event/Upcoming.js
@@ -1,45 +1,55 @@
-import React from 'react'
-import "./Event.css"
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
-
-
-function Upcoming({anime}) {
+import React from "react";
+import "./Event.css";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import { useState, useEffect } from "react";
+function Upcoming({ anime }) {
   // const[data1,setData]=useState([]);
-  console.log(anime)
-  
-  
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 600);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
-    <div className='card1'>
-    <div className='card-ele'>
-      <div>
-        <div className='upcoming-img' style={{ backgroundImage: `url(${anime.topImage})` }}>
-        {/* <img src={anime.topImage} alt="" >
+    <div className="card1">
+      <div className="card-ele">
+        <div>
+          <div
+            className="upcoming-img"
+            style={{ backgroundImage: `url(${anime.topImage})` }}
+          >
+            {/* <img src={anime.topImage} alt="" >
         </img> */}
-        <div className='date'>
-            <h3>{anime.date}
-            <p>{anime.month}</p>
-            </h3>
-            
+            <div className="date">
+              <h3>
+                {anime.date}
+                <p>{anime.month}</p>
+              </h3>
+            </div>
+
+            <div className="border"></div>
+            <div className="topic">
+              Orientation <br></br>Session
+            </div>
           </div>
-         
-          <div className='border'>
-            
+        </div>
+        <div className="info-box">
+          <div className="text-data">
+            <p>{anime.text}</p>
           </div>
-          <div className='topic'>
-            Orientation <br></br>Session
+          <div className="know-ele" style={{ backgroundColor: anime.color }}>
+            <OpenInNewIcon />
+            <p>Know More</p>
           </div>
         </div>
       </div>
-      <div className='text-data'><p>{anime.text}
-
-        </p></div>
-      <div className='know-ele'  style={{backgroundColor: anime.color}}>
-        <OpenInNewIcon />
-        <p>Know More</p>
-      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Upcoming
+export default Upcoming;
