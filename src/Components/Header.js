567382320aa39aba1245f58c6abc542811a59173
Yash sharma <71271069+Yashsharma1911@users.noreply.github.com>
2023-04-01 16:13:45 +0530

    Update src/Components/Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -40,7 +40,7 @@ export default function Header(props) {
                 color: "#959595",
                 fontSize: "14.74px",
                 fontFamily: "Roboto",
-                paddingTop: "2px",
+                paddingTop: "3px",
               }}
             >
               ITM Gwalior
