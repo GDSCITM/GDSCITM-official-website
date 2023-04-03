e9e4c9f222b01738c1eaf3005fe69d7579ffaced
Praveen <potturipraveen7@gmail.com>
2023-04-03 11:17:50 +0530

    Added anchor element to redirect web page when clicked on join us button

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,80 +10,84 @@ import { useTheme } from "@mui/material/styles";
 import MenuIcon from "@mui/icons-material/Menu";
 
 export default function Header(props) {
-  const classes = useStyles();
-  var theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up("900"));
-  const heading = useMediaQuery(theme.breakpoints.up("sm"));
+	const classes = useStyles();
+	var theme = useTheme();
+	const matches = useMediaQuery(theme.breakpoints.up("900"));
+	const heading = useMediaQuery(theme.breakpoints.up("sm"));
 
-  return (
-    <AppBar
-      position="fixed"
-      style={{
-        background: "#fff",
-        width: "100vw",
-        top: 0,
-        boxShadow: "none",
-        fontFamily: "Roboto",
-      }}
-    >
-      {/* <CssBaseline /> */}
-      <Toolbar>
-        <img src={"images/GDSC LOGO 1.svg"} height={"65px"} />
-        <Typography>
-          <div
-            className={classes.heading}
-            style={{ fontSize: heading ? 24 : 16, fontFamily: "Roboto" }}
-          >
-            <div>Google Developer Student Club</div>
-            <div
-              style={{
-                color: "#959595",
-                fontSize: "14.74px",
-                fontFamily: "Roboto",
-                paddingTop: "3px",
-              }}
-            >
-              ITM Gwalior
-            </div>
-          </div>
-        </Typography>
+	return (
+		<AppBar
+			position="fixed"
+			style={{
+				background: "#fff",
+				width: "100vw",
+				top: 0,
+				boxShadow: "none",
+				fontFamily: "Roboto",
+			}}>
+			{/* <CssBaseline /> */}
+			<Toolbar>
+				<img src={"images/GDSC LOGO 1.svg"} height={"65px"} />
+				<Typography>
+					<div
+						className={classes.heading}
+						style={{
+							fontSize: heading ? 24 : 16,
+							fontFamily: "Roboto",
+						}}>
+						<div>Google Developer Student Club</div>
+						<div
+							style={{
+								color: "#959595",
+								fontSize: "14.74px",
+								fontFamily: "Roboto",
+								paddingTop: "3px",
+							}}>
+							ITM Gwalior
+						</div>
+					</div>
+				</Typography>
 
-        <Grid
-          direction="row"
-          justifyContent="flex-end"
-          alignItems="center"
-          style={{
-            display: matches ? "flex" : "none",
-            flexGrow: 2,
-            justifyContent: "flex-end",
-          }}
-        >
-          <Grid className={classes.link}>Home</Grid>
-          <Grid className={classes.link}>About</Grid>
-          <Grid className={classes.link}>Event</Grid>
-          <Grid className={classes.link}>Teams</Grid>
+				<Grid
+					direction="row"
+					justifyContent="flex-end"
+					alignItems="center"
+					style={{
+						display: matches ? "flex" : "none",
+						flexGrow: 2,
+						justifyContent: "flex-end",
+					}}>
+					<Grid className={classes.link}>Home</Grid>
+					<Grid className={classes.link}>About</Grid>
+					<Grid className={classes.link}>Event</Grid>
+					<Grid className={classes.link}>Teams</Grid>
 
-          <Grid className={classes.link}>
-            <Button variant="contained" style={{ backgroundColor: "#2785FC" }}>
-              Join Us
-            </Button>
-          </Grid>
-        </Grid>
+					<Grid className={classes.link}>
+						<Button
+							variant="contained"
+							style={{ backgroundColor: "#2785FC" }}>
+							<a
+								href="https://gdsc.community.dev/institute-of-technology-and-management-gwalior/"
+								target="_blank">
+								Join Us
+							</a>
+						</Button>
+					</Grid>
+				</Grid>
 
-        <Grid
-          direction="row"
-          justifyContent="flex-end"
-          alignItems="center"
-          style={{
-            display: matches ? "none" : "flex",
-            flexGrow: 2,
-            justifyContent: "flex-end",
-            color: "#000",
-          }}
-        >
-          <MenuIcon />
-        </Grid>
-      </Toolbar>
-    </AppBar>
-  );
+				<Grid
+					direction="row"
+					justifyContent="flex-end"
+					alignItems="center"
+					style={{
+						display: matches ? "none" : "flex",
+						flexGrow: 2,
+						justifyContent: "flex-end",
+						color: "#000",
+					}}>
+					<MenuIcon />
+				</Grid>
+			</Toolbar>
+		</AppBar>
+	);
 }
