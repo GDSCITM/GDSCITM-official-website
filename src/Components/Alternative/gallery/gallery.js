7a29665aa2e82acf0b3a6614c7d9bbe026811f26
sumitshinde-84 <shindesumit217@gmail.com>
2023-04-08 14:34:01 +0530

    add gallery fetch images function

--- src/Components/Alternative/gallery/gallery.js
+++ src/Components/Alternative/gallery/gallery.js
@@ -1,50 +1,61 @@
-import styles from "./gallery.module.css"
+import React, { useState, useEffect } from 'react';
+import styles from './gallery.module.css';
 
 export default function Gallery() {
-    return (
-        <div className={styles.main}>
-             <div className={styles.mainHolderTwoItemDesign}>
-                    <img src="images/assets/readmeImages/gallery.png" alt="dot green" />
-                </div>
-            <div className={styles.mainHolder}>
-                <div className={styles.mainHolderOne}>
-                    <div className={styles.mainHolderOneItem}>
-                        <div className={styles.mainHolderOneItemHeader}>
-                            <p style={{ background: "#FE2C25" }}></p>
-                            <p style={{ background: "#2785FC" }}></p>
-                            <p style={{ background: "#FBBC12" }}></p>
-                        </div>
-                        <div className={styles.mainHolderOneItemContainer}>
-                            <img src="images/gallery/2.jpeg" alt="test" />
-                            <img src="images/gallery/1.jpeg" alt="test" />
-                            <img src="images/gallery/2.jpeg" alt="test" />
-                            <img src="images/gallery/1.jpeg" alt="test" />
-                            <img src="images/gallery/2.jpeg" alt="test" />
-                            <img src="images/gallery/1.jpeg" alt="test" />
-                            <img src="images/gallery/2.jpeg" alt="test" />
-                            <img src="images/gallery/1.jpeg" alt="test" />
-                        </div>
-                    </div>
-                </div>
-                <div className={styles.mainHolderTwo}>
-                    <div className={styles.mainHolderTwoItem}>
-                        <div className={styles.mainHolderTwoItemHeading}>
-                            <h2>GDSC ITM</h2>
-                            <p>Beginning of infinity</p>
-                        </div>
-                        <div className={styles.mainHolderTwoItemContainer}>
-                            <p>GDSC's primary aim lies in empowering students with development skills and other technical abilities for growth and advancement. We believe in innovation and evolution. For students to grow their skills to tackle problems and to bring about change.
-                                <br />
-                                <br />
-                                The club is intended as a space for students to try out new ideas and collaborate to solve.</p>
-                        </div>
+  const [fetchedImages, setFetchedImages] = useState([]);
 
-                    </div>
+  // Fetch images asynchronously
+  const fetchImages = async () => {
+    try {
+      const response = await fetch('https://github.com/GDSCITM/GDSC-dataStore/blob/main/Gallery/data.json');
+      const data = await response.json();
+      setFetchedImages(data.images);
+    } catch (error) {
+      console.error('Failed to fetch images:', error);
+    }
+  };
 
-                </div>
-                
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.mainHolderTwoItemDesign}>
+        <img src="images/assets/readmeImages/gallery.png" alt="dot green" />
+      </div>
+      <div className={styles.mainHolder}>
+        <div className={styles.mainHolderOne}>
+          <div className={styles.mainHolderOneItem}>
+            <div className={styles.mainHolderOneItemHeader}>
+              <p style={{ background: '#FE2C25' }}></p>
+              <p style={{ background: '#2785FC' }}></p>
+              <p style={{ background: '#FBBC12' }}></p>
             </div>
-           
+            <div className={styles.mainHolderOneItemContainer}>
+              {fetchedImages.map((image, index) => (
+                <img key={index} src={image.src} alt={image.alt} />
+              ))}
+            </div>
+          </div>
         </div>
-    )
+        <div className={styles.mainHolderTwo}>
+          <div className={styles.mainHolderTwoItem}>
+            <div className={styles.mainHolderTwoItemHeading}>
+              <h2>GDSC ITM</h2>
+              <p>Beginning of infinity</p>
+            </div>
+            <div className={styles.mainHolderTwoItemContainer}>
+              <p>
+                GDSC's primary aim lies in empowering students with development skills and other technical abilities for growth and advancement. We believe in innovation and evolution. For students to grow their skills to tackle problems and to bring about change.
+                <br />
+                <br />
+                The club is intended as a space for students to try out new ideas and collaborate to solve.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
